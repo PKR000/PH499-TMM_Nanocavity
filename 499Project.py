ed62d7dbf5b18e8de2d7efaf1f9d283e91df3817
PKR000 <148403508+PKR000@users.noreply.github.com>
2023-10-30 11:48:48 -0500

    Add files via upload

--- 499Project.py
+++ 499Project.py
@@ -1,4 +1,67 @@
 
+
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+import tmm
+import tmm.examples
+import matplotlib.pyplot as plt
+
+#index of refraction; we would like to have some continuous function describing the complex nk values.
+#if not possible, import data bases
+
+inf = "inf"
+
+#basic read-in function to grab material properties (but I dont like pandas DataFrames so convert to np arrays)
+#the CSV files have the form lambda(.001nm), N, iK
+try:
+    SiO2_data = pd.read_csv("SiO2 for sensitivity.csv")
+    Ag_data = pd.read_csv("Ag for sensitivity.csv")
+except FileNotFoundError:
+    print("File not found.")
+
+SiO2_data = SiO2_data.to_numpy(float)
+Ag_data = Ag_data.to_numpy(float)
+print(SiO2_data)
+print(Ag_data)
+
+
+#main function
+def Calculate():
+    
+    d_top = 35 #nm, top layer thickness
+    d_mid = 125 #nm, test number
+
+    #index of refraction of material: wavelength in nm versus index. This is for read-in files.
+    #interpolates data into continuous function.
+    material_nk_mid = SiO2_data    #test array
+    material_nk_fn_mid = interp1d((material_nk_mid[:,0]*1000),
+                              material_nk_mid[:,1] + material_nk_mid[:,2]*1j, kind='quadratic')
+
+    material_nk_top = Ag_data    #test array
+    material_nk_fn_top = interp1d((material_nk_mid[:,0]*1000),
+                              material_nk_mid[:,1] + material_nk_top[:,2]*1j, kind='quadratic')
+
+    #d_list is thickness of materials, lambda_list is 
+    d_list = [inf,d_top, d_mid, inf] #in nm
+    lambda_list = np.linspace(380, 800, 400) #in nm, this returns array of z evenly spaced numbers from x to y.
+    T_list = []
+    
+    for lambda_vac in lambda_list:
+        n_list = [1,material_nk_fn_top(lambda_vac) , material_nk_fn_mid(lambda_vac), 1]  #for each wavelength, solve material function and add to an N list
+        T_list.append(tmm.coh_tmm('s', n_list, d_list, 0, lambda_vac)['R'])
+    
+    plt.figure()
+    plt.plot(lambda_list, T_list)
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Fraction of power reflected')
+    plt.title('Reflectivity at normal incidence')
+    plt.show()
+
+Calculate()
+
+#_______________notes below______________________
+
 #--To simulate a Fabry-Perot Nanocavity(FPN) analytically using Transfer Matrix Method(TMM)--
  
 #assuming some structure of X interfaces and mediums we can collapse all the interactions into one transfer matrix describing energy in and out from either side of the surface.
@@ -32,12 +95,3 @@
 #the Matrix M is formulated by multiplying all of the matrices that describe the interactions through mediums and interfaces.
 
 #M[] = D01[] * P1[] * D12[] * P2[] * D23[] * P3[]
-
-
-
-import scipy as sp
-import numpy as np
-import colorpy as cp
-import math
-
-
